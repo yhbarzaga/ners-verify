d706888f3d9753bea75f915b0e2c2b6f250b9fdd
Yoelmy Hernandez <dryoe90@gmail.com>
2024-02-14 16:46:20 -0300

    Issue-5: Added initial db model for staff, documents and type

---
+++ app/models/documents.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, ForeignKey, DECIMAL, UUID
+
+from app.db.base_class import Base
+
+
+class Document(Base):
+    """Database model definition for documents."""
+
+    __tablename__ = "documents"
+
+    uid: str = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    total: float = Column(DECIMAL(7, 2), default=0, comment="Total to be refunded")
+    staff_id: str = Column(UUID(as_uuid=True), ForeignKey("staff.uid"), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<User uid: {self.uid}>"
