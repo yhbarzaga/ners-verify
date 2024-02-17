1bed284015afa9e1ee2224ec12422c997508a274
Yoelmy Hernandez <dryoe90@gmail.com>
2024-02-17 08:10:17 -0300

    Issue-25: Update ruff rules

--- app/domain/__init__.py
+++ app/domain/__init__.py
@@ -1,7 +1,2 @@
-from .document import (
-    DocumentAttrs,
-    DocumentDomain,
-    DocumentCreateDomain,
-    DocumentUpdateDomain,
-)  # noqa
+from .document import DocumentAttrs, DocumentDomain, DocumentCreateDomain, DocumentUpdateDomain  # noqa: F401
 from .staff import StaffAttrs, StaffDomain, StaffCreateDomain, StaffUpdateDomain  # noqa
